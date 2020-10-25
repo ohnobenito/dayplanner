b4b3eff3787b9070396c8c8a8e75a07f22d2528c
Benito Gonzalez <ohnobenito@gmail.com>
2020-10-25 12:49:37 -0500

    pseudocode what needs to be done

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,4 +1,21 @@
-let NowMoment = moment();
 
+//DISPLAY TIME AT TOP OF PAGE
+let NowMoment = moment();
 let eDisplayMoment = document.getElementById('currentDay');
-eDisplayMoment.innerHTML = NowMoment.format('[Today is] dddd [the] Do [of] MMMM-YYYY');
+eDisplayMoment.innerHTML = NowMoment.format('[Today is] dddd [the] Do [of] MMMM YYYY');
+
+// ADD FUNCTION TO DISPLAY TIME OF DAY IN EACH TIMEBLOCK DIV
+let nowHour = moment();
+let eDisplayTime = document.getElementsByClassName("hour col-3");
+eDisplayTime.document.createElement("h2");
+eDisplayTime.innerHTML = nowHour.format('YYYY');
+
+//CREATE FUNCTION TO DETERMINE TIME OF DAY
+
+    //IF STATEMENT FOR CURRENT TIME, TIME BLOCK IS GREEN
+
+    //IF STATEMENT FOR FUTURE TIME, TIME BLOCK IS LIGHTER
+
+//ADD FUNCTION FOR WHEN SAVE BUTTON IS CLICKED, INPUT IN TEXT AREA SAVES
+
+//IF PAGE IS REFRESHED, INPUT STAYS ON PAGE
