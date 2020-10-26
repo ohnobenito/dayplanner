41aedb585199497f38db75e70875170616d92fef
Benito Gonzalez <ohnobenito@gmail.com>
2020-10-26 15:10:52 -0500

    added function to check time and change div color based on time of day

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,21 +1,35 @@
-
+$(document).ready(function() {
 //DISPLAY TIME AT TOP OF PAGE
 let NowMoment = moment();
 let eDisplayMoment = document.getElementById('currentDay');
-eDisplayMoment.innerHTML = NowMoment.format('[Today is] dddd [the] Do [of] MMMM YYYY');
+eDisplayMoment.innerHTML = NowMoment.format('[Today is <br>] dddd [the] Do [of] MMMM YYYY');
 
-// ADD FUNCTION TO DISPLAY TIME OF DAY IN EACH TIMEBLOCK DIV
-let nowHour = moment();
-let eDisplayTime = document.getElementsByClassName("hour col-3");
-eDisplayTime.document.createElement("h2");
-eDisplayTime.innerHTML = nowHour.format('YYYY');
 
 //CREATE FUNCTION TO DETERMINE TIME OF DAY
+let hour = parseInt(moment().format('HH'));
+console.log(hour);
 
-    //IF STATEMENT FOR CURRENT TIME, TIME BLOCK IS GREEN
+    $("textarea").each(function () {
+        let time = parseInt($(this).attr("name"));
 
-    //IF STATEMENT FOR FUTURE TIME, TIME BLOCK IS LIGHTER
+        //IF STATEMENT FOR IF TIME IS IN PAST, TURN GREY
+        if (time < hour) {
+            $(this).addClass("past");
+        }
+        //IF STATEMENT FOR FUTURE TIME, TIME BLOCK IS LIGHTER
+        if (time > hour) {
+            $(this).addClass("future");
+        }
+        //IF STATEMENT FOR CURRENT TIME, TIME BLOCK IS GREEN
+        if (time === hour) {
+            $(this).addClass("present");
+        }
+    })
+    
 
 //ADD FUNCTION FOR WHEN SAVE BUTTON IS CLICKED, INPUT IN TEXT AREA SAVES
 
 //IF PAGE IS REFRESHED, INPUT STAYS ON PAGE
+
+/// TEXT AREA; TIME BLOCK; HOUR; SAVE BUTTON
+})
