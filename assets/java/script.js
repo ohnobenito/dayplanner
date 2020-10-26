3a9397fc5fa2b08e3d8d071d8c1633976b682f56
Benito Gonzalez <ohnobenito@gmail.com>
2020-10-26 16:35:27 -0500

    got local storage to work! might look into an easier way to do it?

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
+
 //DISPLAY TIME AT TOP OF PAGE
 let NowMoment = moment();
 let eDisplayMoment = document.getElementById('currentDay');
 eDisplayMoment.innerHTML = NowMoment.format('[Today is <br>] dddd [the] Do [of] MMMM YYYY');
 
 
+let $nineEl = $("#nine");
+let $tenEl = $("#ten");
+let $elevenEl = $("#eleven");
+let $twelveEl = $("#twelve");
+let $oneEl = $("#one");
+let $twoEl = $("#two");
+let $threeEl = $("#three");
+let $fourEl = $("#four");
+let $fiveEl = $("#five");
+
+
 //CREATE FUNCTION TO DETERMINE TIME OF DAY
 let hour = parseInt(moment().format('HH'));
-console.log(hour);
+
 
     $("textarea").each(function () {
         let time = parseInt($(this).attr("name"));
@@ -16,11 +28,11 @@ console.log(hour);
         if (time < hour) {
             $(this).addClass("past");
         }
-        //IF STATEMENT FOR FUTURE TIME, TIME BLOCK IS LIGHTER
+        //IF STATEMENT FOR FUTURE TIME, TIME BLOCK IS DIFFERENT COLOR
         if (time > hour) {
             $(this).addClass("future");
         }
-        //IF STATEMENT FOR CURRENT TIME, TIME BLOCK IS GREEN
+        //IF STATEMENT FOR CURRENT TIME, TIME BLOCK IS DIFFERENT COLOR
         if (time === hour) {
             $(this).addClass("present");
         }
@@ -28,8 +40,26 @@ console.log(hour);
     
 
 //ADD FUNCTION FOR WHEN SAVE BUTTON IS CLICKED, INPUT IN TEXT AREA SAVES
+$(".saveBtn").on("click", function() {
+    localStorage.setItem("nine", (($nineEl.val())));
+    localStorage.setItem("ten", (($tenEl.val())));
+    localStorage.setItem("eleven", (($elevenEl.val())));
+    localStorage.setItem("twelve", (($twelveEl.val())));
+    localStorage.setItem("one", (($oneEl.val())));
+    localStorage.setItem("two", (($twoEl.val())));
+    localStorage.setItem("three", (($threeEl.val())));
+    localStorage.setItem("four", (($fourEl.val())));
+    localStorage.setItem("five", (($fiveEl.val())));
+})
 
 //IF PAGE IS REFRESHED, INPUT STAYS ON PAGE
-
-/// TEXT AREA; TIME BLOCK; HOUR; SAVE BUTTON
+$("#nine").append(localStorage.getItem("nine"));
+$("#ten").append(localStorage.getItem("ten"));
+$("#eleven").append(localStorage.getItem("eleven"));
+$("#twelve").append(localStorage.getItem("twelve"));
+$("#one").append(localStorage.getItem("one"));
+$("#two").append(localStorage.getItem("two"));
+$("#three").append(localStorage.getItem("three"));
+$("#four").append(localStorage.getItem("four"));
+$("#five").append(localStorage.getItem("five"));
 })
